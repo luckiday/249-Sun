03f75e15920e68f54d2ea17c720a013f4000b747
luckiday <guoyunqi@gmail.com>
2017-06-03 23:45:49 -0700

    baseline method

--- src/fastClustering.R
+++ src/fastClustering.R
@@ -1,14 +1,13 @@
 rm(list = ls())
 library('igraph')
-setwd(getwd())
-edges = read.csv("../edges_clean.txt", header = FALSE, sep = "\t")
-edges = edges + 1 
+edges = read.csv("../data/top5CommunitiesEdges.csv", header = FALSE, sep = ",")
+edges = edges
 # read communities
 con  <- file("../top_5_communities.txt", open = "r")
 communities <- list()
 while (length(oneLine <- readLines(con, n = 1, warn = FALSE)) > 0) {
   myVector <- (strsplit(oneLine, "\t"))
-  myVector <- list(as.numeric(myVector[[1]]) + 1)
+  myVector <- list(as.numeric(myVector[[1]]))
   communities <- c(communities,myVector)
 } 
 close(con)
