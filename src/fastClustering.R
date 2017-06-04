f7fcedf8285e199b64c58e482248f50bf078db25
luckiday <guoyunqi@gmail.com>
2017-06-03 20:10:19 -0700

    ignore

--- src/fastClustering.R
+++ src/fastClustering.R
@@ -1,1 +1,19 @@
+rm(list = ls())
+library('igraph')
 setwd(getwd())
+edges = read.csv("../edges_clean.txt", header = FALSE, sep = "\t")
+edges = edges + 1 
+# read communities
+con  <- file("../top_5_communities.txt", open = "r")
+communities <- list()
+while (length(oneLine <- readLines(con, n = 1, warn = FALSE)) > 0) {
+  myVector <- (strsplit(oneLine, "\t"))
+  myVector <- list(as.numeric(myVector[[1]]) + 1)
+  communities <- c(communities,myVector)
+} 
+close(con)
+
+#form the graph based on the edgelist
+edges = as.matrix(edges)
+g=graph.data.frame(edges,directed = FALSE)
+fastgreedy.community(g)
