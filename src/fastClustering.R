036bcc3c8820513973a6df771b0d51c717c3e69e
luckiday <guoyunqi@gmail.com>
2017-06-04 23:38:01 -0700

    fast graph clustering method in mama

--- src/fastClustering.R
+++ src/fastClustering.R
@@ -1,12 +1,11 @@
 rm(list = ls())
 library('igraph')
 edges = read.csv("../data/top5CommunitiesEdges.csv", header = FALSE, sep = ",")
-edges = edges
 # read communities
-con  <- file("../top_5_communities.txt", open = "r")
+con  <- file("../data/top5Communities.txt", open = "r")
 communities <- list()
 while (length(oneLine <- readLines(con, n = 1, warn = FALSE)) > 0) {
-  myVector <- (strsplit(oneLine, "\t"))
+  myVector <- (strsplit(oneLine, ","))
   myVector <- list(as.numeric(myVector[[1]]))
   communities <- c(communities,myVector)
 } 
